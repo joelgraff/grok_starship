9e238917ddba426db653fa7e52a74c7650a5b61d
Joel Graff <monograff76@gmail.com>
2025-03-28 22:33:53 -0500

    expanded ship layout

--- src/simulation.py
+++ src/simulation.py
@@ -3,25 +3,22 @@ import simpy
 from mesa import Model
 from src.crew import CrewAgent
 from src.tasks import TaskManager
+from src.ship import Starship
 
 class StarshipSimulation(Model):
     def __init__(self, crew_data, ship_layout):
         super().__init__()
         self.env = simpy.Environment()
-        # self.schedule = RandomActivation(self)  # Scheduler for agents
-        self.ship_layout = ship_layout
+        self.ship = Starship(ship_layout)
         self.task_manager = TaskManager(self)
         self.running = True
 
-        # Initialize crew agents and add to scheduler
         for i, data in enumerate(crew_data):
             agent = CrewAgent(i, self, data)
             self.agents.add(agent)
-            #self.schedule.add(agent)
-        self.crew = self.agents  # Store agents for easy access
+        self.crew = self.agents
 
     def step(self):
-    #"""Advance the simulation by one step."""
-        self.agents.shuffle_do("step")  # This will skip dead agents after removal
+        self.agents.shuffle_do("step")
         self.task_manager.update()
         self.env.run(until=self.env.now + 1)
